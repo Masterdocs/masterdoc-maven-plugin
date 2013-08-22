48d8c568fa5f269fc84df1b31113cfb88be974bc
Pierre Leresteux <pierre.leresteux@gmail.com>
2013-08-22 14:27:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ masterdoc-maven-plugin
 ======================
 
 Plugin Maven to generate documentation for JAX-RS API
+
+
+Use with : 
+
+<plugin>
+    <groupId>com.masterdoc</groupId>
+    <artifactId>masterdoc-maven-plugin</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <executions>
+     <execution>
+      <phase>install</phase>
+      <goals>
+       <goal>masterdoc</goal>
+      </goals>
+     </execution>
+    </executions>
+</plugin>
---
+++ src/main/java/com/masternaut/platform/masterdoc/pojo/ResourceEntry.java
@@ -0,0 +1,128 @@
+package com.masternaut.platform.masterdoc.pojo;
+
+import static javax.ws.rs.core.MediaType.WILDCARD;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: pleresteux
+ */
+public class ResourceEntry implements Serializable {
+  private String                 verb;
+  private String                 path;
+  private List<Param>            queryParams;
+  private List<Param>            pathParams;
+  private Serializable           requestEntity;
+  private Serializable           responseEntity;
+  private String                 mediaTypeConsumes;
+  private String                 mediaTypeProduces;
+  private CamelConsumeAnnotation CamelConsume;
+  private String                 fullPath;
+  private boolean                async;
+
+  public ResourceEntry() {
+    this(WILDCARD, WILDCARD);
+  }
+
+  public ResourceEntry(String mediaTypeConsumes, String mediaTypeProduces) {
+    this.mediaTypeConsumes = mediaTypeConsumes;
+    this.mediaTypeProduces = mediaTypeProduces;
+    this.pathParams = new ArrayList<Param>();
+    this.queryParams = new ArrayList<Param>();
+    this.async = false;
+  }
+
+  public String getVerb() {
+    return verb;
+  }
+
+  public void setVerb(String verb) {
+    this.verb = verb;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public List<Param> getQueryParams() {
+    return queryParams;
+  }
+
+  public void setQueryParams(List<Param> queryParams) {
+    this.queryParams = queryParams;
+  }
+
+  public List<Param> getPathParams() {
+    return pathParams;
+  }
+
+  public void setPathParams(List<Param> pathParams) {
+    this.pathParams = pathParams;
+  }
+
+  public Serializable getRequestEntity() {
+    return requestEntity;
+  }
+
+  public void setRequestEntity(Serializable requestEntity) {
+    this.requestEntity = requestEntity;
+  }
+
+  public Serializable getResponseEntity() {
+    return responseEntity;
+  }
+
+  public void setResponseEntity(Serializable responseEntity) {
+    this.responseEntity = responseEntity;
+  }
+
+  public String getMediaTypeConsumes() {
+    return mediaTypeConsumes;
+  }
+
+  public void setMediaTypeConsumes(String mediaTypeConsumes) {
+    this.mediaTypeConsumes = mediaTypeConsumes;
+  }
+
+  public String getMediaTypeProduces() {
+    return mediaTypeProduces;
+  }
+
+  public void setMediaTypeProduces(String mediaTypeProduces) {
+    this.mediaTypeProduces = mediaTypeProduces;
+  }
+
+  public CamelConsumeAnnotation getCamelConsume() {
+    return CamelConsume;
+  }
+
+  public void setCamelConsume(CamelConsumeAnnotation camelConsume) {
+    CamelConsume = camelConsume;
+  }
+
+  public void setFullPath(String fullPath) {
+    this.fullPath = fullPath;
+  }
+
+  public String getFullPath() {
+    return fullPath;
+  }
+
+  public void setAsync(boolean async) {
+    this.async = async;
+  }
+
+  public boolean isAsync() {
+    return async;
+  }
+
+  public String calculateUniqKey() {
+    return this.verb + ">>" + this.path;
+  }
+}
